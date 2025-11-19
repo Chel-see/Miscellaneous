acd96e68e100ca20d9c62da17f11cafc0fafc9c0
Sanjiv Jahleel Mohan <sanjiv.mohan1@gmail.com>
2025-11-19 19:30:12 -0400

    Task #45: Update User model

--- App/models/user.py
+++ App/models/user.py
@@ -7,20 +7,27 @@ class User(db.Model):
     username =  db.Column(db.String(20), nullable=False, unique=True)
     password = db.Column(db.String(256), nullable=False)
     role = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    phone_number = db.Column(db.String(20), nullable=True)
 
     student = db.relationship('Student', backref='user', uselist=False)
     employer = db.relationship('Employer', backref='user', uselist=False)
     staff = db.relationship('Staff', backref='user', uselist=False)
     
-    def __init__(self, username, password, role):
+    def __init__(self, username, password, role, email, phone_number=None):
         self.username = username
         self.set_password(password)
         self.role = role
+        self.email = email
+        self.phone_number = phone_number
 
     def get_json(self):
         return{
             'id': self.id,
-            'username': self.username
+            'username': self.username,
+            'role': self.role,
+            'email': self.email,
+            'phone_number': self.phone_number
         }
 
     def set_password(self, password):
