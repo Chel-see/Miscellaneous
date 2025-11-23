fd180034fa8b98b37b84e290dbd9334ade3cf6a0
Chelsea R <chelsearoopnarine@yahoo.com>
2025-11-23 15:40:16 -0400

    #12 Implement new Context Class

---
+++ App/models/context.py
@@ -0,0 +1,24 @@
+
+class Context:
+    def __init__(self, initial_state):  # first state the content would be assigned to. we can switch form this state to others
+        self.state = initial_state  # i dont need to pre-declare the state attribute in python
+        self.state.setContext(self)   
+
+    def setState(self, state):
+        self.state = state
+        self.state.setContext(self)
+
+    def next(self):
+        return self.state.next()
+
+    def previous(self):
+        return self.state.previous()
+
+    def withdraw(self):
+        return self.state.withdraw()
+
+    def getStateName(self):
+        return self.state.getStateName()
+    
+    def getMatchedCompanies(self):
+        return self.state.getMatchedCompanies()
